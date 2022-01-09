a03231010eca4822ce6b8cca9f1af98a8737973c
DESKTOP-CTG31MG\admin <hanscarreon0898@gmail.com>
2022-01-09 23:26:49 +0800

    create of audit

--- audit/create.php
+++ audit/create.php
@@ -1,41 +1,48 @@
 <?php
 
+
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-$conn = mysqli_connect('localhost', 'u934258034_vaccsched', 'VaccSched091321', 'u934258034_vaccsched');
 
+$servername = "localhost";
+$username = "u943769473_immunouser";
+$password = "xI4t#e^s@";
+$dbname = "u943769473_immuno";
 
-$tz_object = new \DateTimeZone('Asia/Manila');
-$datetime = new \DateTime();
-$datetime->setTimezone($tz_object);
-$datetime->format('Y\-m\-d\ H:i:s');
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
+
+
 
 $al_userID = $_POST['al_userID'];
 $al_details = $_POST['al_details'];
-$al_created_at = $datetime->format('Y\-m\-d\ H:i:s');
 
-$sql = "INSERT INTO audit_logs (al_userID, al_details, al_created_at)
-VALUES ( 
-'" . $al_userID . "', 
-'" . $al_details . "',
-'" . $al_created_at . "'
-)";
 
-if (isset($_POST['save_logs'])) {
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+if($al_userID == null){
+    echo 'anuthorized person';
+    return;
+
+}else if($al_details == null){
+    echo 'details is required!';
+    return;
 }
 
 
+$sql = "INSERT INTO audit_logs (al_userID, al_details) VALUES ('" . $al_userID . "', '" . $al_details . "')";
 
+// $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+// VALUES ('John', 'Doe', 'john@example.com')";
+$save = $conn->query($sql);
+if ($save === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-$conn->close();
+// $conn->close();
